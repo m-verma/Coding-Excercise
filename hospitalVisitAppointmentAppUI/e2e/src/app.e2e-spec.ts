7a0a685ac93638621e1e3008ac05ed1534930f39
Mukesh Verma/MAXIMUS <100111@maximus.com>
2019-09-23 13:12:34 -0700

    updated readme

--- hospitalVisitAppointmentAppUI/e2e/src/app.e2e-spec.ts
+++ hospitalVisitAppointmentAppUI/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to appointment-management!');
+    expect(page.getTitleText()).toEqual('Welcome to appointment management!');
   });
 
   afterEach(async () => {
